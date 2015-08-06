07ff826f2b32916aa40d5e92dc0409621fef5656
Eduard Albu <eduardalbu@gmail.com>
2015-08-06 22:07:18 +0300

    Работаю над получением данных с сервера

--- src/main/java/com/shkap/ui/LoginActivity.java
+++ src/main/java/com/shkap/ui/LoginActivity.java
@@ -1,90 +1,68 @@
 package com.shkap.ui;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.login.LoginResult;
 import com.shkap.R;
+import com.shkap.model.ViewInitializer;
+import com.shkap.social.FBManager;
 import com.shkap.social.VKManager;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.Signature;
-
-public class LoginActivity extends Activity implements View.OnClickListener {
+public class LoginActivity extends ViewInitializer implements View.OnClickListener {
 
     private Intent mMainIntent;
+    private Button mGoogleButton;
+    private Button mVKButton;
+    private CallbackManager callbackManager;
+    private com.facebook.login.widget.LoginButton mFacebookButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-        FacebookSdk.sdkInitialize(this);
+        initViews();
+        callbackManager = FBManager.getCallbackManager();
+        FBManager fbManager = new FBManager(LoginActivity.this);
         VKManager vkManager = new VKManager(this, LoginActivity.this);
         mMainIntent = new Intent(this, MainActivity.class);
+
         if (vkManager.isLoggedIn()) {
             startActivity(mMainIntent);
             finish();
         }
-        CallbackManager callbackManager = new CallbackManager() {
-            @Override
-            public boolean onActivityResult(int i, int i1, Intent intent) {
-                Log.i("TAG", FacebookSdk.getClientToken());
-                return true;
-            }
-        };
-        Button mGoogleButton = (Button) findViewById(R.id.ggp_loginBtn);
-        mGoogleButton.setOnClickListener(this);
-
-        com.facebook.login.widget.LoginButton mFacebookButton = (com.facebook.login.widget.LoginButton)
-                findViewById(R.id.fb_loginBtn);
-        mFacebookButton.setReadPermissions("user_friends");
-        mFacebookButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException e) {
-
-            }
-        });
-
-
-
-        Button mVKButton = (Button) findViewById(R.id.vk_loginBtn);
-        mVKButton.setOnClickListener(this);
-    }
-
-    public void makeToasts(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         VKManager.setAccesToken();
+        callbackManager.onActivityResult(requestCode, resultCode, data);
         Log.i("TAG", VKManager.getAccesToken());
         startActivity(mMainIntent);
         finish();
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void initToolbar() {}
+
+    @Override
+    protected void initViews() {
+        mGoogleButton = (Button) findViewById(R.id.ggp_loginBtn);
+        mGoogleButton.setOnClickListener(this);
+
+        mFacebookButton = (com.facebook.login.widget.LoginButton)
+                findViewById(R.id.fb_loginBtn);
+        mFacebookButton.setReadPermissions("user_friends");
+        mFacebookButton.registerCallback(FBManager.getCallbackManager(),
+                FBManager.getLoginResul());
+
+        mVKButton = (Button) findViewById(R.id.vk_loginBtn);
+        mVKButton.setOnClickListener(this);
     }
 
     @Override
@@ -98,5 +76,9 @@ public class LoginActivity extends Activity implements View.OnClickListener {
                 makeToasts("Welcome");
                 break;
         }
+    }
+
+    public void makeToasts(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
